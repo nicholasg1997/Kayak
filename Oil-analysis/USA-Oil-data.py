639c9bd767c60ff4c828d8cacca3e1ca07064773
nicholasg1997 <63173211+nicholasg1997@users.noreply.github.com>
2023-05-08 13:57:03 -0600

    cleaned code

--- Oil-analysis/USA-Oil-data.py
+++ Oil-analysis/USA-Oil-data.py
@@ -3,13 +3,15 @@ WTI oil production
 source EIA.gov
 """
 
+import os
+
 import pandas as pd
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 API_KEY = os.getenv("EIA_TOKEN")
+
 
 def get_request(url):
     """"
@@ -30,8 +32,9 @@ def get_request(url):
 
     return df
 
-def MBBL_production (frequency="monthly", API_KEY=API_KEY, start_date="2015-12"):
-    """""
+
+def mbbl_production(frequency="monthly", api_key=API_KEY, start_date="2015-12"):
+    """
     Get the data from EIA.gov on crude oil production my month or year
     returns monthly MBBL data
     :param 
@@ -40,27 +43,26 @@ def MBBL_production (frequency="monthly", API_KEY=API_KEY, start_date="2015-12")
     start_date: str, "YYYY-MM"
     :return 
     pandas dataframe
-    """""
+    """
 
-    url = f"https://api.eia.gov/v2/petroleum/crd/crpdn/data/?api_key={API_KEY}&\
+    url = f"https://api.eia.gov/v2/petroleum/crd/crpdn/data/?api_key={api_key}&\
     frequency={frequency}&start={start_date}&data[0]=value&sort[0][column]=period&\
     sort[0][direction]=desc&offset=0"
 
     df = get_request(url)
 
-
     df['barrels_per_month'] = df.apply(lambda x: int(x['value']) * 30 if x['units'] == 'MBBL/D'
                                        else int(x['value']), axis=1)
 
-    df_MBBL = df['barrels_per_month']
-    df_MBBL = df_MBBL.groupby('period').sum()
-    df_MBBL = df_MBBL.sort_index(ascending=False)
+    df_mbbl = df['barrels_per_month']
+    df_mbbl = df_mbbl.groupby('period').sum()
+    df_mbbl = df_mbbl.sort_index(ascending=False)
 
-    return df_MBBL
+    return df_mbbl
 
 
-def crude_oil_stocks(frequency="monthly", API_KEY=API_KEY, start_date="2015-12"):
-    """""
+def crude_oil_stocks(frequency="monthly", api_key=API_KEY, start_date="2015-12"):
+    """
     Get the data from EIA.gov on crude oil stocks by month or year
     returns monthly stocks in MBBL
     :param 
@@ -69,9 +71,9 @@ def crude_oil_stocks(frequency="monthly", API_KEY=API_KEY, start_date="2015-12")
     start_date: str, "YYYY-MM"
     :return 
     pandas dataframe
-    """""
+    """
 
-    url = f"https://api.eia.gov/v2/petroleum/stoc/cu/data/?api_key={API_KEY}&\
+    url = f"https://api.eia.gov/v2/petroleum/stoc/cu/data/?api_key={api_key}&\
         frequency={frequency}&start={start_date}&data[0]=value&sort[0][column]=period&\
         sort[0][direction]=desc&offset=0"
 
@@ -84,17 +86,18 @@ def crude_oil_stocks(frequency="monthly", API_KEY=API_KEY, start_date="2015-12")
 
     return storage_data
 
-def imports_exports(API_KEY=API_KEY):
-    """""
+
+def imports_exports(api_key=API_KEY):
+    """
     Get the data from EIA.gov on crude oil imports and exports
     returns weekly imports and exports in MBBL
     :param
     API_KEY: str, API key from EIA.gov
     :return
     pandas dataframe
-    """"
+    """
 
-    url = f"https://api.eia.gov/v2/petroleum/move/wkly/data/?api_key={API_KEY}&\
+    url = f"https://api.eia.gov/v2/petroleum/move/wkly/data/?api_key={api_key}&\
     frequency=weekly&data[0]=value&facets[product][]=EP00&facets[product][]=EPC0&sort[0][column]=period&\
     sort[0][direction]=desc&offset=0&length=5000"
 
@@ -104,14 +107,3 @@ def imports_exports(API_KEY=API_KEY):
     imports = df[df['process-name'].str.contains('imports', case=False)]
 
     return exports, imports
-
-
-
-
-
-
-
-
-
-
-
