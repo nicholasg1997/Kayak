e1e064a295e973b2fa67c5f8720b4d2aaa6306bd
nicholasg1997 <63173211+nicholasg1997@users.noreply.github.com>
2023-05-10 10:03:44 -0600

    added short term prices

--- Oil_analysis/FRED_oil_data.py
+++ Oil_analysis/FRED_oil_data.py
@@ -6,8 +6,8 @@ from full_fred.fred import Fred
 import pandas as pd
 from datetime import datetime
 
-fred = Fred('FRED_key.txt')
-today = datetime.now().strftime("%Y-%m-%d")
+
+
 
 
 def to_numeric(val):
@@ -24,15 +24,23 @@ def to_numeric(val):
         return float(val)
 
 
-def get_fred_data(series_id, start_date="2000-01-01"):
+def get_fred_data(series_id, start_date="2000-01-01", api_key=None):
     """
     Get the data from FRED
     :param
     series_id: str, series id from FRED
     start_date: str, "YYYY-MM-DD"
+    api_key: str, API key from FRED
     :return
     pandas dataframe
     """
+
+    if api_key is None:
+        fred = Fred('FRED_key.txt')
+    else:
+        fred = Fred(api_key)
+
+    today = datetime.now().strftime("%Y-%m-%d")
 
     data = fred.get_series_df(series_id, observation_start=start_date, observation_end=today)
     data = data[['date', 'value']]
